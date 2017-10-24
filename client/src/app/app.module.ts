0c3ecc6f8f0695016cc0c72d4ae96883b8cafb2e
shivangi.5.sharma <shivangi.5.sharma@niit-tech.com>
2017-10-24 20:52:37 +0530

    updated server and updated client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
@@ -8,36 +7,25 @@ import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import { HttpModule} from '@angular/http';
 import { ErrorhandlingComponent} from './errorhandling/errorhandling.component';
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    ErrorhandlingComponent
-  ],
-  imports: [
-    BrowserModule,RouterModule,AppRoutingModule,FormsModule,HttpModule
-=======
-import { AppRoutingModule} from './app-routing.module';
-import { HttpModule} from '@angular/http';
-import { AppComponent } from './app.component';
-import { FormsModule} from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
 import  {RegisterService} from './shared/register.service';
 import {MailotpService} from './shared/mailotp.service';
 import  {MailverificationComponent} from './mailverification/mailverification.component';
 
 
-
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    ErrorhandlingComponent,
     RegisterComponent,MailverificationComponent
+
   ],
   imports: [
-    BrowserModule,AppRoutingModule,FormsModule,HttpModule
->>>>>>> 1628e70b3390ba7cd3b060295fcfbb506afcc660
+    BrowserModule,RouterModule,AppRoutingModule,FormsModule,HttpModule
   ],
   providers: [RegisterService,MailotpService],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
